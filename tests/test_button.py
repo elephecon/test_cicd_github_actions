706688bb1a96258e55f5791acc02878602227fda
ksaiw <none>
2024-04-15 13:56:05 +0600

    updated test, workflow dispatch

--- tests/test_button.py
+++ tests/test_button.py
@@ -16,6 +16,12 @@ def browser(chrome_options):
     return chrome_browser
 
 
+@pytest.mark.smoke
 def test_button_exist(browser):
     browser.get('https://www.qa-practice.com/elements/button/simple')
     assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
+
+@pytest.mark.regression
+def test_button_exist_2(browser):
+    browser.get('https://www.qa-practice.com/elements/button/like_a_button')
+    assert browser.find_element(By.PARTIAL_LINK_TEXT, 'Click').is_displayed()
